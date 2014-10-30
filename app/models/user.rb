c6c1cc3b6987876dc38d0a7eb158ac6b836070ce
Mike Martinson <mike.j.martinson@gmail.com>
2014-10-30 10:18:27 -0700

    Google oauth implemented, uid and provider added to users table.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,22 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  # :confirmable, :lockable, :timeoutable and 
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :omniauthable, :omniauth_providers => [:google_oauth2]
+
+  def self.find_for_google_oauth2(access_token, signed_in_resource=nil)
+      data = access_token.info
+      user = User.where(:email => data["email"]).first
+
+      unless user
+          user = User.create(name: data["name"],
+             email: data["email"],
+             password: Devise.friendly_token[0,20]
+          )
+      end
+      user
+  end
 
   def display_name
     return name if self.name
